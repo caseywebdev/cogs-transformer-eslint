da3d8c5fef2d4d1266b7c0d4964f5cf795b85c7f
Casey Foster <c@sey.me>
2015-04-09 17:24:20 -0500

    Use executeOnText to save a file load, 1.0.4

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var eslint = require('eslint');
 
 module.exports = function (file, options, cb) {
   var cli = new eslint.CLIEngine(options);
-  var ers = cli.executeOnFiles([file.path]).results[0].messages;
-  for (var i = 0, l = ers.length; i < l; ++i) {
-    var er = ers[i];
+  var ers = cli.executeOnText(file.buffer.toString()).results[0].messages;
+  var i, l, er;
+  for (i = 0, l = ers.length; i < l; ++i) {
+    er = ers[i];
     if (!er.fatal && er.severity < 2) continue;
     return cb(new Error(
       file.path + ': line ' + er.line + ', column ' + er.column + ', ' +
